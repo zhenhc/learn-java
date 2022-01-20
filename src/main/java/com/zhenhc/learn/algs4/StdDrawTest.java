7f2351ffb0f11dd65711a7768fdacc7c4464486c
zhenhc <1308197806@qq.com>
2022-01-20 11:19:18 +0800

    添加algs4的jar包，添加测试类

---
+++ src/main/java/com/zhenhc/learn/algs4/StdDrawTest.java
@@ -0,0 +1,17 @@
+package com.zhenhc.learn.algs4;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class StdDrawTest {
+    public static void main(String[] args) {
+        int N = 100;
+        StdDraw.setXscale(0, N); //StdDraw就是algs4中的类
+        StdDraw.setYscale(0, N*N);
+        StdDraw.setPenRadius(.01);
+        for(int i = 1; i <= N; ++i) {
+            StdDraw.point(i, i);
+            StdDraw.point(i, i*i);
+            StdDraw.point(i, i*Math.log(i));
+        }
+    }
+}
---
+++ src/main/java/com/zhenhc/learn/hutool/CaptchaUtilDemo.java
@@ -0,0 +1,13 @@
+package com.zhenhc.learn.hutool;
+
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
+import cn.hutool.http.server.HttpServerRequest;
+
+import java.io.IOException;
+
+public class CaptchaUtilDemo {
+
+
+
+}
